dec950a5ea62ffbf8f27562faec5221d033334f7
Yessica Gomez <yessica.gomez@ucsf.edu>
2018-01-26 14:17:12 -0800

    IT WORKED. Now to RUN edge case tests

--- test/test_algs.py
+++ test/test_algs.py
@@ -8,45 +8,43 @@ def test_pointless_sort():
     assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
 
 # generic tests for algorithm logic #
-
-def test_bubblesort():
-    # generate random vector of length 100
-    x = np.random.rand(100)
-    algs.bubblesort(x)
-
-def test_quicksort():
-    # generate random vector of length 100
-    x = np.random.rand(100)
-    algs.quicksort(x, 0, int(len(x)-1))
-
-def test_main():
-    # generate random vector of length 100
-    x = np.random.rand(100)
-    algs.main(x)
+# def test_bubblesort():
+#     # generate random vector of length 100
+#     x = np.random.rand(100)
+#     algs.bubblesort(x)
+#
+# def test_quicksort():
+#     # generate random vector of length 100
+#     x = np.random.rand(100)
+#     algs.quicksort(x, 0, int(len(x)-1))
+#
+# def test_main():
+#     # generate random vector of length 100
+#     x = np.random.rand(100)
+#     algs.main(x)
 
 # testing specific edge cases #
+def test_empty():
+    # generate random vector of length 0
+    x = np.empty([1,1])
+    main(x) # check if it fails if the vector is empty
 
-# def test_empty():
-#     # generate random vector of length 0
-#     x = np.empty([1,1])
-#     main(x) # check if it fails if the vector is empty
-#
-# def test_single():
-#     # generate random vector containing 1 element
-#     x = np.random.rand(1,1)
-#     main(x)
-#
-# def test_duplicated():
-#     # generate random vector containing 100 elements
-#     x = np.random.rand(1, 100)
-#     # add 2 identical elements to it (duplicates)
-#     x.extend([9,9])
-#     main(x)
-#
-# def odd_even():
-#     # generate random vector containing 99 elements
-#     x = np.random.rand(1, 99)
-#     main(x)
-#     # generate random vector containing 999 elements
-#     x = np.random.rand(1, 999)
-#     main(x)
+def test_single():
+    # generate random vector containing 1 element
+    x = np.random.rand(1,1)
+    main(x)
+
+def test_duplicated():
+    # generate random vector containing 100 elements
+    x = np.random.rand(1, 100)
+    # add 2 identical elements to it (duplicates)
+    x.extend([9,9])
+    main(x)
+
+def odd_even():
+    # generate random vector containing 99 elements
+    x = np.random.rand(1, 99)
+    main(x)
+    # generate random vector containing 999 elements
+    x = np.random.rand(1, 999)
+    main(x)
