0cc6c5c7107945f6ad465850d7fbcf3024ac6782
Yessica Gomez <yessica.gomez@ucsf.edu>
2018-01-26 12:17:29 -0800

    Fixing main.py

--- example/__main__.py
+++ example/__main__.py
@@ -1,4 +1,17 @@
 # this file is only called when the package is called from the command line
-from .run import run_stuff
+import numpy as np
+from .algs import quicksort, bubblesort
+
+def run_stuff():
+    """
+    This function is called in `__main__.py`
+    """
+    print("This is `run()` from ", __file__)
+
+    x = np.random.rand(10)
+    print("Unsorted input: ", x)
+
+    print("Bubble sort output: ", bubblesort(x))
+    print("Quick sort output: ", quicksort(x))
 
 run_stuff()
