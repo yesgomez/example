42132b0860ef10c953206f1d2d92adcb75ee8733
Yessica Gomez <yessica.gomez@ucsf.edu>
2018-01-26 11:35:37 -0800

    Does this env still work?

--- example/__main__.py
+++ example/__main__.py
@@ -1,5 +1,4 @@
-# this file is only called when the package is called from the command
-# line
+# this file is only called when the package is called from the command line
 from .run import run_stuff
 
 run_stuff()
