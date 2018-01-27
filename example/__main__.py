d9917e9e82c50816d57e754395fb477ff9f82de4
Yessica Gomez <yessica.gomez@ucsf.edu>
2018-01-27 06:26:58 -0800

    Better count data formatting

--- example/__main__.py
+++ example/__main__.py
@@ -1,5 +1,6 @@
 # this file is only called when the package is called from the command line
 import numpy as np
+import pandas as pd
 from example import algs
 # from .algs import quicksort, bubblesort, main
 
@@ -17,6 +18,7 @@ def run_stuff():
     return bcounts, qcounts
 
 bdata, qdata = run_stuff()
-print ("The counts for each algorithm are ", bdata, qdata)
+print ("The counts for bubblesort are\n", pd.DataFrame(bdata, index=[np.arange(100,1100,100)], columns=['assignments', 'conditionals']))
+print ("The counts for quicksort are\n", pd.DataFrame(qdata, index=[np.arange(100,1100,100)], columns=['assignments', 'conditionals']))
 
 # def plot_stuff():
