b7e84de1adc43bc07ee9f29cb40f7078f106dd96
Yessica Gomez <yessica.gomez@ucsf.edu>
2018-01-27 07:16:43 -0800

    Making sure everything is there

--- example/__main__.py
+++ example/__main__.py
@@ -21,25 +21,27 @@ bdata, qdata = run_stuff()
 print ("The counts for bubblesort are\n", pd.DataFrame(bdata, index=[np.arange(100,1100,100)], columns=['assignments', 'conditionals']))
 print ("The counts for quicksort are\n", pd.DataFrame(qdata, index=[np.arange(100,1100,100)], columns=['assignments', 'conditionals']))
 
-def plot_stuff(data, color, title, comp):
-    x = []
+def plot_stuff(data, title, comp):
+    x = np.arange(100,1100,100)
     y = []
+    z = []
     for ea in data: # split the array into x and y values for plotting
-        x.append(ea[0])
-        y.append(ea[1])
-    plt.plot(x, y, color) # plot with specified color
+        y.append(ea[0])
+        z.append(ea[1])
+    plt.plot(x, y, 'r') # plot with specified color
+    plt.plot(x, z, 'g')
     plt.title(title)
     # adding comparison plot
-    if comp = 'n2':
-        m = np.arange(0,x[-1],1) # define n^2
+    if comp is 'n2':
+        m = np.arange(100,1100,100) # define n^2
         n = m**2
-    elif comp = 'nlogn':
-        m = np.arange(0,x[-1],1) # define nlogn
+    elif comp is 'nlogn':
+        m = np.arange(100,1100,100) # define nlogn
         n = m * np.log(m)
     else:
-        print "no such line available... yet"
-    plt.plot(m, n, '--')
+        print ("no such line available... yet")
+    plt.plot(m, n, 'r--')
     plt.show()
 
-plot_stuff(bdata, 'r', 'BubbleSort', 'n2')
-plot_stuff(qdata, 'b', 'QuickSort', 'nlogn')
+plot_stuff(bdata, 'BubbleSort', 'n2')
+plot_stuff(qdata, 'QuickSort', 'nlogn')
