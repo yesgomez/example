4c3ec1d103ccd8728b3dcac02670b7b1160cc79d
Yessica Gomez <yessica.gomez@ucsf.edu>
2018-01-27 07:02:17 -0800

    Graphing is present and accounted for.

--- example/__main__.py
+++ example/__main__.py
@@ -1,8 +1,8 @@
 # this file is only called when the package is called from the command line
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from example import algs
-# from .algs import quicksort, bubblesort, main
 
 def run_stuff():
     print("This is run_stuff() from ", __file__)
@@ -21,4 +21,25 @@ bdata, qdata = run_stuff()
 print ("The counts for bubblesort are\n", pd.DataFrame(bdata, index=[np.arange(100,1100,100)], columns=['assignments', 'conditionals']))
 print ("The counts for quicksort are\n", pd.DataFrame(qdata, index=[np.arange(100,1100,100)], columns=['assignments', 'conditionals']))
 
-# def plot_stuff():
+def plot_stuff(data, color, title, comp):
+    x = []
+    y = []
+    for ea in data: # split the array into x and y values for plotting
+        x.append(ea[0])
+        y.append(ea[1])
+    plt.plot(x, y, color) # plot with specified color
+    plt.title(title)
+    # adding comparison plot
+    if comp = 'n2':
+        m = np.arange(0,x[-1],1) # define n^2
+        n = m**2
+    elif comp = 'nlogn':
+        m = np.arange(0,x[-1],1) # define nlogn
+        n = m * np.log(m)
+    else:
+        print "no such line available... yet"
+    plt.plot(m, n, '--')
+    plt.show()
+
+plot_stuff(bdata, 'r', 'BubbleSort', 'n2')
+plot_stuff(qdata, 'b', 'QuickSort', 'nlogn')
--- example/algs.py
+++ example/algs.py
@@ -1,5 +1,4 @@
 import numpy as np
-import timeit
 from random import *
 
 def pointless_sort(x):
