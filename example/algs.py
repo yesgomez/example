9d7c33223d252e3f5f15f1db989c438be6c5460a
Yessica Gomez <yessica.gomez@ucsf.edu>
2018-06-06 14:36:57 -0700

    Added descriptions to algs functions

--- example/algs.py
+++ example/algs.py
@@ -2,75 +2,79 @@ import numpy as np
 from random import *
 
 def pointless_sort(x):
-    """
-    This function always returns the same values to show how testing
-    works, check out the `test/test_alg.py` file to see.
-    """
-    return np.array([1,2,3])
+	"""
+	This function always returns the same values to show how testing
+	works, check out the `test/test_alg.py` file to see.
+	"""
+	return np.array([1,2,3])
 
 def bubblesort(l):
-    """
-    This function sorts the list (L = n) by comparing adjacent
+	"""
+	This function sorts the list (L = n) by comparing adjacent
 	elements pairwise. It goes through the list (n-1) times.
-    """
-    # define counters for assignments & conditionals
-    asn = 0
-    con = 0
-    # actual sorting #
-    for passnum in range(0,len(l)-1,1):
-        con += 1 # conditional counter
-        for i in range(passnum):
-            con += 1 # conditional counter
-            if l[i]>l[i+1]:
-                con += 1 # conditional counter
-                temp = l[i]
-                l[i] = l[i+1]
-                l[i+1] = temp
-                asn += 1 # assignment counter
-    return asn, con # get counts for assignments and conditionals
+	"""
+	# define counters for assignments & conditionals
+	asn = 0
+	con = 0
+	# actual sorting 
+	for passnum in range(0,len(l)-1,1):
+		con += 1 # conditional counter
+		for i in range(passnum):
+			con += 1 # conditional counter
+			if l[i]>l[i+1]:
+				con += 1 # conditional counter
+				temp = l[i]
+				l[i] = l[i+1]
+				l[i+1] = temp
+				asn += 1 # assignment counter
+	return asn, con # get counts for assignments and conditionals
 
 acount = 0
 ccount = 0
 def quicksort(l, left, right):
+	m = left
+	n = right
 	"""
-    This function sorts the list (L = n) by successively splitting
-	the list
-    """
-    # define counters for assignments & conditionals
-    global acount
-    global ccount
-    # setting left and right #
-    m = left
-    n = right
-    # actual sorting #
-    pivot = l[int((left + right)/2)] # define the pivot point
-    while m <= n:
-        ccount += 1
-        while l[m] < pivot:
-            m += 1
-            ccount += 1 # conditional counter
-        while l[n] > pivot:
-            n -= 1
-            ccount += 1 # conditional counter
-        if m <= n:
-            # tmp = l[m]
-            l[m], l[n] = l[n], l[m]
-            # l[n] = tmp
-            m += 1
-            n -= 1
-            acount += 1 # assignment counter
-            ccount += 1 # conditional counter
-    # recursive bit #
-    if left < n:
-        quicksort(l, left, n)
-    if (m < right):
-        quicksort(l, m, right)
-    return acount, ccount # get counts for assignments and conditionals
+	This function sorts the list (L = n) by successively splitting
+	the list at an updated pivot point and comparing the two ends to
+	each other and the pivot point.
+	"""
+	# define counters for assignments & conditionals
+	global acount
+	global ccount
+	# setting left and right 
+	m = left
+	n = right
+	# actual sorting 
+	pivot = l[int((left + right)/2)] # define the pivot point
+	while m <= n:
+		ccount += 1
+		while l[m] < pivot:
+			m += 1
+			ccount += 1 # conditional counter
+		while l[n] > pivot:
+			n -= 1
+			ccount += 1 # conditional counter
+		if m <= n:
+			l[m], l[n] = l[n], l[m]
+			m += 1
+			n -= 1
+			acount += 1 # assignment counter
+			ccount += 1 # conditional counter
+	# recursive bit 
+	if left < n:
+		quicksort(l, left, n)
+	if (m < right):
+		quicksort(l, m, right)
+	return acount, ccount # get counts for assignments and conditionals
 
-# the main subroutine runs both sorting algorithms and return the results for a vector l #
 def main(l):
-    asnb, conb = bubblesort(l)
-    asnq, conq = quicksort(l, 0, int(len(l)-1))
-    print(l, "\n%s and %s are assignments and conditionals for bubbleSort, respectively.\n" %(asnb, conb))
-    print(l, "\n%s and %s are assignments and conditionals for quickSort, respectively.\n" %(asnq, conq))
-    return asnb, conb, asnq, conq
+	"""
+	This main subroutine runs both sorting algorithms and return the
+	results for a vector l.
+	"""
+	asnb, conb = bubblesort(l)
+	asnq, conq = quicksort(l, 0, int(len(l)-1))
+	print(l, "\n%s and %s are assignments and conditionals for bubbleSort, respectively.\n" %(asnb, conb))
+	print(l, "\n%s and %s are assignments and conditionals for quickSort, respectively.\n" %(asnq, conq))
+	return asnb, conb, asnq, conq
